61aaf61f9410cb291c450116645212c601e67f43
padams12 <paul.adams@testpro.io>
2026-06-17 08:21:27 -0700

    Homework 19
    Homework 19

---
+++ Homework19.java/src/Main.java
@@ -0,0 +1,27 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+    // to see how IntelliJ IDEA suggests fixing it.
+    IO.println(String.format("Hello and welcome!"));
+
+    for (int i = 1; i <= 5; i++) {
+        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+        IO.println("i = " + i);
+    }
+}
+import org.Assert;
+import org.testing.annotations.Test;
+public class Homework19 extends Basetest  {
+    @Test
+    public void deletePlaylist() throws InterruptedException{
+        String ExpectedPlaylistDeletedMessage = "Deleted Playlist\*Test Pro Playlist.\**;
+                provideEmail(*Paul.Adams@testpro.io*0;
+                providePassword(*Amanda$12*);
+                clickSubmit();
+                openPlaylist();
+                clickDeletePlatlistbtn();
+                Assert.assertEqual(getDeletedPlaylistMsg(),ExpectedPlaylistDeletedMessage);
+    }
+}
