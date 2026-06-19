7041e8a128e37072fe60f56d8c44447be5ea802a
padams12 <paul.adams@testpro.io>
2026-06-19 09:58:16 -0700

    Homework20

---
+++ Homework20.java/src/Main.java
@@ -0,0 +1,35 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+    // to see how IntelliJ IDEA suggests fixing it.
+    IO.println(String.format("Hello and welcome!"));
+
+    for (int i = 1; i <= 5; i++) {
+        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+        IO.println("i = " + i);
+    }
+}
+
+import.org.testing.Assert;
+import.org.test.annotations.Test;
+public class Homework 19 extends BaseTest{
+public void deletePlaylist() ThrowsinterruptionException
+    String ExpectedPlaylistDelete="Deleted playlist\*TestPro Playlist.\**;
+wait=new WebDriverWait(driver.Duration.of Seconds (4));
+
+
+            provide Email(*Paul.Adams@testpro.io*);
+            provide Password(*Amanda12*);
+            clickSubmit();
+            openPlaylist();
+
+            wait=newWebDriverWait(driver.Duration.of.Seconds(4));
+            clickDeletePlaylistbtn();
+            Assert.assertEquals(getDeletedPlaylistMsg(),ExpectedPlaylistDeleteMessage
+                           }
+
+                     }
+
+
