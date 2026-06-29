ebd61509f9b4cf042a8132f0ac2f157534529bb9
padams12 <paul.adams@testpro.io>
2026-06-28 19:09:55 -0700

    Submitting homework16

---
+++ src/main/java/Homework16.java
@@ -0,0 +1,28 @@
+import java.time.Duration;
+
+public class Homework16 {public class Homework16 {
+}
+    @test
+
+    public void  registration Homework16() {
+        ChromeOptions options =newChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+    }
+
+    WebDriver driver = newChromeDriver(options);
+    driver,
+
+    Homework16().
+
+    Homework16().
+
+    Homework16(Duration, of seconds(10));
+
+    String url= "https:// go.koel.app";
+   driver.get(url);
+
+    Web Element registration= driver.findElement (By.css Selector ("[href = 'registration']"));
+   registration.click();
+    String registration = "https// go.koel.app/registration";
+   Assert.assertEquals(driver.getCurrenturl();)
+}
