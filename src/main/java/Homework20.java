3edf62d64413b83476d280255f9893bdfd61ab08
padams12 <paul.adams@testpro.io>
2026-06-28 22:44:07 -0700

    Submitting Homework20.java

---
+++ src/main/java/Homework20.java
@@ -0,0 +1,21 @@
+public class Homework20
+{import.org.testing.Assert;
+import.org.test.annotations.Test;
+    public class Homework 19 extends BaseTest{
+        public void deletePlaylist() ThrowsinterruptionException
+        String ExpectedPlaylistDelete="Deleted playlist\*TestPro Playlist.\**;
+        wait=new WebDriverWait(driver.Duration.of Seconds (4));
+
+
+        provide Email(*Paul.Adams@testpro.io*);
+        provide Password(*Amanda12*);
+        clickSubmit();
+        openPlaylist();
+
+        wait=newWebDriverWait(driver.Duration.of.Seconds(4));
+        clickDeletePlaylistbtn();
+        Assert.assertEquals(getDeletedPlaylistMsg(),ExpectedPlaylistDeleteMessage
+    }
+
+
+}
